672640e3f3941824582cd065bd6ec7104d5e36ab
Ben Jung <benjungkz@gmail.com>
2020-05-13 15:43:59 -0400

    Updates

---
+++ precache-manifest.115f2278e5eb76b7658df8bb9387cc6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd377b20258d533c0b483db0a201157",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b13cd80ea9642a5754",
+    "url": "/movie_app_2020/static/js/main.1877c94e.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
